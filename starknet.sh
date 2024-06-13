08ca1efc989bef790a6a81ec4178e46c5a15d6f9
Cantas4dart <adeniranpeter19@gmail.com>
2024-06-13 12:59:26 +0100

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://rpc.starknet.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-4993fd64-bd9b-4eae-a355-98798fda7594/"
 
 OUTPUT_DIR="logs"
 
